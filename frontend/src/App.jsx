77ed1405483dee0fe8981d09cd87e0e1b38e77d3
Christian Neal <nealparedes776@gmail.com>
2026-07-11 08:59:36 +0800

    Added Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./pages/hero/Hero";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <main>
         <Hero />
       </main>
+      <Footer />
     </>
   );
 }
