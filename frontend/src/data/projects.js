63bf6a4d65b21e3693072d3991b3fe280ec773ca
Christian Neal <nealparedes776@gmail.com>
2026-07-11 18:51:34 +0800

    Adding page indicator

---
+++ frontend/src/data/projects.js
@@ -0,0 +1,33 @@
+const projects = [
+  {
+    title: "Smart Library System",
+    description:
+      "A web-based library platform for seamless borrowing, tracking, reservations, approvals, and administration.",
+    image: "/projects/L7_0lwHq.jpg",
+    tags: ["MongoDB", "Express", "React", "Node.js", "Bootstrap", "AWS S3"],
+    liveUrl: "https://smartls.onrender.com/",
+    githubUrl: "https://github.com/ThomasDelamort/SmartLibrarySystem",
+    year: "2024",
+  },
+  {
+    title: "Spotify Clone",
+    description:
+      "A fullstack music streaming app with real-time playback, admin dashboard, and social listening features.",
+    image: "/projects/image.png",
+    tags: [
+      "TypeScript",
+      "MongoDB",
+      "Tailwind",
+      "Express",
+      "React",
+      "NodeJS",
+      "Cloudinary",
+      "Clerk",
+    ],
+    liveUrl: "#",
+    githubUrl: "#",
+    year: "2024",
+  },
+];
+
+export default projects;
