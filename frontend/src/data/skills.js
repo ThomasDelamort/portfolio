11b4379f9464aeb483fd5a1caaa92a2bd2f75955
Christian Neal <nealparedes776@gmail.com>
2026-07-13 18:14:17 +0800

    clean up

---
+++ frontend/src/data/skills.js
@@ -0,0 +1,77 @@
+import {
+  SiReact,
+  SiTypescript,
+  SiJavascript,
+  SiTailwindcss,
+  SiBootstrap,
+  SiHtml5,
+  SiNodedotjs,
+  SiExpress,
+  SiMongodb,
+  SiClerk,
+  SiCloudinary,
+  SiGit,
+  SiGithub,
+  SiVite,
+  SiInsomnia,
+  SiPostgresql,
+  SiGnubash,
+  SiRender,
+} from "react-icons/si";
+import { FaAws, FaJava, FaPython, FaDocker, FaCss3Alt } from "react-icons/fa";
+import { TbBrandCpp } from "react-icons/tb";
+import { GrArchlinux, GrMysql } from "react-icons/gr";
+
+const skillGroups = [
+  {
+    category: "Frontend",
+    skills: [
+      { name: "React", icon: SiReact },
+      { name: "Tailwind CSS", icon: SiTailwindcss },
+      { name: "Bootstrap", icon: SiBootstrap },
+      { name: "HTML 5", icon: SiHtml5 },
+      { name: "CSS 3", icon: FaCss3Alt },
+    ],
+  },
+  {
+    category: "Backend",
+    skills: [
+      { name: "Node.js", icon: SiNodedotjs },
+      { name: "Express", icon: SiExpress },
+      { name: "AWS", icon: FaAws },
+      { name: "Clerk", icon: SiClerk },
+      { name: "Cloudinary", icon: SiCloudinary },
+    ],
+  },
+  {
+    category: "Database",
+    skills: [
+      { name: "MongoDB", icon: SiMongodb },
+      { name: "MySQL", icon: GrMysql },
+      { name: "PostgreSQL", icon: SiPostgresql },
+    ],
+  },
+  {
+    category: "Tools and Platforms",
+    skills: [
+      { name: "Git", icon: SiGit },
+      { name: "GitHub", icon: SiGithub },
+      { name: "Docker", icon: FaDocker },
+      { name: "Insomnia", icon: SiInsomnia },
+      { name: "Vite", icon: SiVite },
+      { name: "Render", icon: SiRender },
+    ],
+  },
+  {
+    category: "Programming Languages",
+    skills: [
+      { name: "Java", icon: FaJava },
+      { name: "JavaScript", icon: SiJavascript },
+      { name: "C++", icon: TbBrandCpp },
+      { name: "Python", icon: FaPython },
+      { name: "Bash", icon: SiGnubash },
+    ],
+  },
+];
+
+export default skillGroups;
--- frontend/src/pages/skills/Skills.jsx
+++ frontend/src/pages/skills/Skills.jsx
@@ -21,58 +21,7 @@ import {
 import { FaAws, FaJava, FaPython, FaDocker, FaCss3Alt } from "react-icons/fa";
 import { TbBrandCpp } from "react-icons/tb";
 import { GrArchlinux, GrMysql } from "react-icons/gr";
-
-const skillGroups = [
-  {
-    category: "Frontend",
-    skills: [
-      { name: "React", icon: SiReact },
-      { name: "Tailwind CSS", icon: SiTailwindcss },
-      { name: "Bootstrap", icon: SiBootstrap },
-      { name: "HTML 5", icon: SiHtml5 },
-      { name: "CSS 3", icon: FaCss3Alt },
-    ],
-  },
-  {
-    category: "Backend",
-    skills: [
-      { name: "Node.js", icon: SiNodedotjs },
-      { name: "Express", icon: SiExpress },
-      { name: "AWS", icon: FaAws },
-      { name: "Clerk", icon: SiClerk },
-      { name: "Cloudinary", icon: SiCloudinary },
-    ],
-  },
-  {
-    category: "Database",
-    skills: [
-      { name: "MongoDB", icon: SiMongodb },
-      { name: "MySQL", icon: GrMysql },
-      { name: "PostgreSQL", icon: SiPostgresql },
-    ],
-  },
-  {
-    category: "Tools and Platforms",
-    skills: [
-      { name: "Git", icon: SiGit },
-      { name: "GitHub", icon: SiGithub },
-      { name: "Docker", icon: FaDocker },
-      { name: "Insomnia", icon: SiInsomnia },
-      { name: "Vite", icon: SiVite },
-      { name: "Render", icon: SiRender },
-    ],
-  },
-  {
-    category: "Programming Languages",
-    skills: [
-      { name: "Java", icon: FaJava },
-      { name: "JavaScript", icon: SiJavascript },
-      { name: "C++", icon: TbBrandCpp },
-      { name: "Python", icon: FaPython },
-      { name: "Bash", icon: SiGnubash },
-    ],
-  },
-];
+import skillGroups from "../../data/skills";
 
 const Skills = () => {
   return (
@@ -118,5 +67,5 @@ const Skills = () => {
     </section>
   );
 };
-
+("");
 export default Skills;
