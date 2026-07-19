71376d390059905893dee30ca8d78e99bb557927
Christian Neal <nealparedes776@gmail.com>
2026-07-19 23:39:48 +0800

    updates

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -28,6 +28,26 @@ const projects = [
     githubUrl: "https://github.com/ThomasDelamort/SpotifyClone",
     year: "2026",
   },
+  {
+    title: "Portfolio Website",
+    description:
+      "The fullstack application that not only showcases my projects but also who I am as a person.",
+    image: "/projects/portfolio-website.png",
+    tags: ["React", "Tailwind"],
+    liveURL: "https://christiannealparedes.onrender.com/",
+    githubURL: "https://github.com/ThomasDelamort/portfolio",
+    year: "2026",
+  },
+  {
+    title: "Will you go out on a date with me",
+    description:
+      "This was a fun fullstack project, a very playful way to ask the girl I like out on a date",
+    image: "/projects/onlyYou.png",
+    tags: ["PostgreSQL", "Express", "React", "NodeJS", "Tailwind", "Neon"],
+    liveURL: "https://will-you-go-out-with-me-zvgh.onrender.com/",
+    githubURL: "https://github.com/ThomasDelamort/willYouGoOutOnADateWithMe",
+    year: "2026",
+  },
 ];
 
 export default projects;
