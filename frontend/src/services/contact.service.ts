55b24c44640039c07dde6fc819f2039db85cca1e
Christian Neal <nealparedes776@gmail.com>
2026-08-01 21:25:34 +0800

    wired up frontend with backend api

--- frontend/src/services/contact.service.ts
+++ frontend/src/services/contact.service.ts
@@ -1,19 +1,21 @@
 import type { ContactForm, ContactResponse } from "../types/contact.types.ts";
 
-const API: string = import.meta.env.API;
+const API: string = import.meta.env.VITE_API_URL;
 
-export async function sendContact( payload: ContactForm ): Promise<ContactResponse> {
-    const response = await fetch(`${API}/contacts`, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(payload),
-    })
+export async function sendContact(
+  payload: ContactForm,
+): Promise<ContactResponse> {
+  const response = await fetch(`${API}/contact`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(payload),
+  });
 
-    const data = await response.json()
+  const data = await response.json();
 
-    if (!response.ok) {
-        throw new Error(data.error);
-    }
+  if (!response.ok) {
+    throw new Error(data.error);
+  }
 
-    return data;
+  return data;
 }
